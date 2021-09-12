0f3a1a66d6fa73dc25a754d24ec890657fe42d7a
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-09-12 07:58:02 +0600

    Edited scripts for java and cpp

--- Compiling_cpp-java-python/runCpp.sh
+++ Compiling_cpp-java-python/runCpp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Filename passed in as argument
+# Filename and flag passed in as argument. Flag is used for recompilation.
 fileName=$1
+flag=$2
 
 # Color Codes
 BLUE='\033[1;34m'
@@ -51,20 +52,27 @@ do_stuff() {
     add_or_delete_lines "dlt"
 }
 
+# To handle case when we need to recompile and run the file, we will check the flag
 # If bin folder doesn't exist, create one and compile the cpp file
 # If it exists, check it has been compiled already or not
 # If it's already compiled just execute the bin file, compile and execute otherwise
-if [ -d "$DIR_OUT" ]
+if [ "$flag" = "recompile" ]
 then
-    if [ -f "$DIR_OUT"/"$fileName".out ]
+    rm -r out/"$fileName".*
+    do_stuff
+else
+if [ -d "$DIR_OUT" ]
     then
-        execute_bin
+        if [ -f "$DIR_OUT"/"$fileName".out ]
+        then
+            execute_bin
+        else
+            do_stuff
+        fi
     else
+        mkdir "$DIR_OUT"
         do_stuff
     fi
-else
-    mkdir "$DIR_OUT"
-    do_stuff
 fi
 
 echo '\n'
--- Compiling_cpp-java-python/runJava.sh
+++ Compiling_cpp-java-python/runJava.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Filename passed in as argument
+# Filename passed in as argument. Flag is used for recompilation.
 fileName=$1
+flag=$2
 
 # Color codes
 BLUE='\033[1;34m'
@@ -28,20 +29,30 @@ execute_bin() {
     cd "$DIR"
 }
 
+# To handle case when we need to recompile and run the file, we will check the flag
 # If bin folder doesn't exist, create one and compile the cpp file
 # If it exists, check it has been compiled already or not
 # If it's already compiled just execute the bin file, compile and execute otherwise
-if [ -d "$DIR_OUT" ]
+if [ "$flag" = "recompile" ]
 then
-    if [ -f "$DIR_OUT"/"$fileName".class ]
+    rm -r out/"$fileName".*
+    compile
+else
+    if [ -d "$DIR_OUT" ]
     then
-        execute_bin
+        if [ -f "$DIR_OUT"/"$fileName".class ]
+        then
+            execute_bin
+        else
+            compile
+        fi
     else
+        mkdir "$DIR_OUT"
         compile
     fi
-else
-    mkdir "$DIR_OUT"
-    compile
 fi
+
+# To shift focus back to the editor
+xdotool key Control_L+period
 
 echo '\n'
