f07eeb034ca13ecdca0c9d4d057276b1cc290161
def-username <defusername@debian-BULLSEYE-live-builder-AMD64>
2024-09-15 05:07:20 +0600

    Removed the ability of the download organizer to be always running in the background

--- organize_download.sh
+++ organize_download.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
 
-while true; do
-	for file in ~/Downloads/*; do
-		if [[ -d "$file" ]]; then
-			continue
-		fi
-		if [[ $file == *.mp3 || $file == *.wav ]]; then
-			mkdir -p ~/Downloads/music
-			mv "$file" ~/Downloads/music/
-		elif [[ $file == *.jpg || $file == *.png || $file == *.jpeg || $file == *.gif || $file == *.webp || $file == *.svg ]]; then
-			mkdir -p ~/Downloads/images
-			mv "$file" ~/Downloads/images/
-		elif [[ $file == *.mp4 || $file == *.mov ]]; then
-			mkdir -p ~/Downloads/videos
-			mv "$file" ~/Downloads/videos/
-		elif [[ $file == *.pdf ]]; then
-			mkdir -p ~/Downloads/pdfs
-			mv "$file" ~/Downloads/pdfs/
-		elif [[ $file == *.zip || $file == *.tar* || $file == *.deb ]]; then
-			mkdir -p ~/Downloads/archives
-			mv "$file" ~/Downloads/archives/
-		elif [[ $file == *.deb ]]; then
-			mkdir -p ~/Downloads/packages
-			mv "$file" ~/Downloads/packages/
-		elif [[ $file == *.exe || $file == *.msi || $file == *.AppImage ]]; then
-			mkdir -p ~/Downloads/executables
-			mv "$file" ~/Downloads/executables/
-		elif [[ $file == *.csv || $file == *.ods || $file == *xls || $file == *.xlsx || $file == *.txt || $file == *.docx || $file == *.doc || $file == *.ppt || $file == *.pptx ]]; then
-			mkdir -p ~/Downloads/docs
-			mv "$file" ~/Downloads/docs/
-		else
-			mkdir -p ~/Downloads/others
-			mv "$file" ~/Downloads/others
-		fi
-	done
-	# run the command automatically every 60 seconds
-	sleep 60
+for file in ~/Downloads/*; do
+	if [[ -d "$file" ]]; then
+		continue
+	fi
+	if [[ $file == *.mp3 || $file == *.wav ]]; then
+		mkdir -p ~/Downloads/music
+		mv "$file" ~/Downloads/music/
+	elif [[ $file == *.jpg || $file == *.png || $file == *.jpeg || $file == *.gif || $file == *.webp || $file == *.svg ]]; then
+		mkdir -p ~/Downloads/images
+		mv "$file" ~/Downloads/images/
+	elif [[ $file == *.mp4 || $file == *.mov || $file == *.mkv || $file == *.webm || $file == *.wmv ]]; then
+		mkdir -p ~/Downloads/videos
+		mv "$file" ~/Downloads/videos/
+	elif [[ $file == *.pdf || $file == *.odf ]]; then
+		mkdir -p ~/Downloads/pdfs
+		mv "$file" ~/Downloads/pdfs/
+	elif [[ $file == *.zip || $file == *.tar* ]]; then
+		mkdir -p ~/Downloads/archives
+		mv "$file" ~/Downloads/archives/
+	elif [[ $file == *.deb ]]; then
+		mkdir -p ~/Downloads/packages
+		mv "$file" ~/Downloads/packages/
+	elif [[ $file == *.exe || $file == *.msi || $file == *.AppImage || $file == *.deb || $file == *.flatpakref ]]; then
+		mkdir -p ~/Downloads/executables
+		mv "$file" ~/Downloads/executables/
+	elif [[ $file == *.csv || $file == *.ods || $file == *xls || $file == *.xlsx || $file == *.txt || $file == *.docx || $file == *.doc || $file == *.ppt || $file == *.pptx ]]; then
+		mkdir -p ~/Downloads/docs
+		mv "$file" ~/Downloads/docs/
+	else
+		mkdir -p ~/Downloads/others
+		mv "$file" ~/Downloads/others
+	fi
 done
