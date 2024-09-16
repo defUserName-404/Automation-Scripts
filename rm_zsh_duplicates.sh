5648b721bcec74c487dc6fea8d40c30ca016ec15
def-username <defusername@debian-BULLSEYE-live-builder-AMD64>
2024-09-16 23:21:07 +0600

    Changed the way the zsh history duplicates are deleted

--- rm_zsh_duplicates.sh
+++ rm_zsh_duplicates.sh
@@ -1,3 +1,9 @@
-cat -n ~/.zsh_history | sort -t ';' -uk2 | sort -nk1 | cut -f2- > ~/.zsh_short_history
+#!/bin/bash
 
-mv ~/.zsh_short_history ~/.zsh_history
+# Automatically deletes the zsh history every 5 minutes
+while true; do
+	cat -n $HOME/.zsh_history | sort -t ';' -uk2 | sort -nk1 | cut -f2- > $HOME/.zsh_short_history
+	mv $HOME/.zsh_short_history $HOME/.zsh_history
+
+	sleep 300
+done
